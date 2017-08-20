bb4950604a91f9eb6e3ea13f4a3eff1ed886e4a7
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-20 17:05:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML='I am ak47';
 var img=document.getElementById('madi');
 var marginLeft=0;
 img.onclick=function(){
-   madi.style.marginLeft = '300px';
+   madi.style.marginLeft = '500px';
 };
