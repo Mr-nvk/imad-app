8f28f98b5c461a6ebf1efe882d6564c492c04cbf
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-20 12:47:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element=document.getElementId('main-text');
 element.innerHTML='new value';
 
-var img=document.getElementId('img');
+var img=document.getElementId('madi');
 img.onclick=function(){
     img.style.mrginleft='100px';
     
