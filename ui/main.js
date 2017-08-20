60e4d764ff2696d9058f7a8bb77bf97bb0624d03
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-20 22:53:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,8 @@
-console.log('Loaded!');
-
-
-var element=document.getElementById('main-text');
-
-element.innerHTML='I am ak47';
-
-
-
-var img=document.getElementById('madi');
-
-var marginLeft=0;
-function moveRight() {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
+var button=document.getElementById('counter');
+button.onclick=function(){
+    
+    counter=counter+1;
+    var span= document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
 };
