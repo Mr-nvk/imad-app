5a6713b93bc4040ae101edda201b878763b4b18d
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-21 05:45:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,5 +21,11 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
-    
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>'; 
+    }
+    var u1 = document.getElementById('namelist');
+    u1.innerHTML = list;
 };
