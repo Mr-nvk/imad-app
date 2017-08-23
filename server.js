f996e12eb73cce2f5a295bc94b766ad3bbe9ebc0
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-24 00:43:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool=require('pg').Pool;
 
-var config={
-    user:'naveenkumawat19952',
-    database:'naveenkumawat19952',
-    host: 'db.imad.hasura-app.io',
-    port:'5432',
-    password: process.emv.DB_PASSWORD
-};
 var app = express();
 app.use(morgan('combined'));
 
@@ -79,7 +71,7 @@ function createTemplate (data) {
         </div>
     </body>
 </html>
-`; 
+`;
 return htmlTemplate;
 }
 
@@ -87,18 +79,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-    
-    pool.query('SELECT*FROM test', function(err,result){
-       if(err){
-           res.status(500).send(err.toString());
-       } 
-       else{
-           res.send(JSON.stringify(result.rows));
-       }
-    });
-});
 var counter=0;
 app.get('/counter',function(req , res){
     counter = counter+1;
@@ -115,27 +95,9 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify(names));
 });
 
-app.get('/article/:articleName', function(req,res){
+app.get('/:articleName', function(req,res){
     var articleName = req.params.articleName;
-    
-    pool.query("SELECT * FROM article WHERE title = "+req.params.articleName,function(err,result){
-       if(err){
-         res.status(500).send(err.toString());  
-       } 
-       else{ 
-       if(result.rows.length=== 0){
-           res.status(404).send('article not found');
-       
-           
-       }
-       else{
-           var articleData= result.rows[0];
-           res.send(createTemplate(articleData));
-       }
-       }
-    });
-    
-   
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
