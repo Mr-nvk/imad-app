93b0b5800344515f81e54a3d1afe8cdc28e8dafd
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-23 05:32:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,11 +125,8 @@ app.get('/article/:articleName', function(req,res){
        else{ 
        if(result.rows.length=== 0){
            res.status(404).send('article not found');
-       
-           
-       }
-       else{
-           var articleData= result.rows[0];
+       } else {
+           var articleData = result.rows[0];
            res.send(createTemplate(articleData));
        }
        }
