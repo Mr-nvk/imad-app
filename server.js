a99c97e1689fbc6126662342ca1204aecfa8021c
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-26 02:14:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
+
 var config = {
     user:'naveenkumawat19952',
     database:'naveenkumawat19952',
@@ -84,6 +86,17 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input,salt){
+    var hashed = crypto.pbkdf25Sync(input, salt, 10000,512, 'sha512' );
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input', function(req,res){
+    var hashedString = hash (req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
 });
 
 var pool = new Pool(config); 
