712d342371ac32fe48c390d056b15161ccee3c61
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-27 09:25:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,14 +103,12 @@ app.post('/create-user', function(req,res){
     
    var salt= crypto.randomBytes(128).toString('hex');
    var dbString = hash(password,salt);
-    pool.query('INSERT INTO "user"(username,password)VALUES($1,$2)',[username,dbString],function(err,result){
+    pool.query('INSERT INTO "user"(username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
         if(err){
-           res.status(500).send(err.toString())
-;         
+           res.status(500).send(err.toString());         
        }
-       
        else{
-           res.send("user successfully created: "+username);
+           res.send("user successfully created: " +username);
        }
     });
 });
