92770b597b605a1793874cd21e9bfcf0b7130da1
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-27 14:14:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,8 +124,8 @@ app.post('/login', function(req,res){
            res.status(500).send(err.toString());         
        }
        else{
-           if(result.rows.length===0){
-             res.send(403).send('username/password id invalid');  
+           if(result.rows.length === 0) {
+             res.send(403).send('username/password is invalid');  
            }else{
              var dbString = result.rows[0].password; 
             var salt = dbString.split('$')[2];
