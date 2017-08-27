55587e5b72540cd3cd7e1274f36b0331ce4e22e4
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-27 19:28:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,24 +119,26 @@ app.post('/login', function(req,res){
     var username = req.body.username;
     var password = req.body.password;
    
-   pool.query('SELECT * FROM  "user" WHERE username = $1', [username],function(err,result){
+   pool.query('SELECT * from "user" username = $1', [username], function(err,result){
         if(err){
            res.status(500).send(err.toString());         
        }
-       else{
+       else {
            if(result.rows.length === 0) {
-             res.send(403).send('username/password is invalid');  
+               res.send(403).send('username/password is invalid');
            }else{
-             var dbString = result.rows[0].password; 
-            var salt = dbString.split('$')[2];
-            var hashedPassword = hash(password,salt);
-            if(hashedPassword === dbString){
-           res.send("credentials created");
-           }else{
-             res.send(403).send('username/password id invalid');   
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               var hashedPassword = hash(password, salt);
+               if (hashedPassword === dbString) {
+                   res.send('credentials correct');
+               }else {
+                   res.send(403).send('username/password is invalid');
+               }
+               
            }
-           }
-       } 
+           
+       }
    }); 
 });
 
