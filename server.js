9204f17142c5908608b4001933a65789e3b91959
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-27 09:40:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,24 +95,6 @@ function hash(input,salt){
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join('@');
 }
 
-app.post('/create-user', function(req,res){
-    
-    
-   var username = req.body.username;
-   var password = req.body.password;
-    
-   var salt= crypto.randomBytes(128).toString('hex');
-   var dbString = hash(password,salt);
-    pool.query('INSERT INTO "user"(username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
-        if(err){
-           res.status(500).send(err.toString());         
-       }
-       else{
-           res.send("user successfully created: " +username);
-       }
-    });
-});
-
 app.get('/hash/:input', function(req,res){
     var hashedString = hash (req.params.input,'this-is-some-random-string');
     res.send(hashedString);
@@ -121,24 +103,19 @@ app.get('/hash/:input', function(req,res){
 app.post('/create-user',function(req,res){
     var username = req.body.username;
     var password = req.body.password;
-   var salt = crypto.getRandomBytes(128).toString('hex');
+   var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
    pool.query('INSERT INTO "user" (username,password)VALUES($1,$2)', [username,dbString],function(err,result){
         if(err){
-           res.status(500).send(err.toString())
-;         
+           res.status(500).send(err.toString());         
        }
-       
        else{
            res.send("User successfully created:" +username);
        }
-       
    });
-    
 });
 
 var pool = new Pool(config); 
-
 app.get('/test-db', function(req,res){
     //return a response
    pool.query('SELECT *  FROM test', function(err,result){
