762cc04e2b13b15ff109fa4e408ad3443c54faff
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-28 13:29:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/hash/:input', function(req,res){
     res.send(hashedString);
 });
 
--app.post('/create-user', function(req,res){
+app.post('/create-user', function(req,res){
      
      
     var username = req.body.username;
