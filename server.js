ea399c2aebc3c589d60289dac39ab9af576d3fed
Mr-nvk <naveenkumawat1995@gmail.com>
2017-08-28 21:43:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
-var bodyParser = require('body-parser');
 
 var config = {
     user:'naveenkumawat19952',
@@ -14,7 +13,6 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParser.json());
 
 var articles = {
     'article-one': {
@@ -91,71 +89,31 @@ app.get('/', function (req, res) {
 });
 
 function hash(input,salt){
-    var hashed = crypto.pbkdf2Sync(input, salt, 1000,512, 'sha512' );
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000,512, 'sha512' );
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join('@');
 }
+
 
 app.get('/hash/:input', function(req,res){
     var hashedString = hash (req.params.input,'this-is-some-random-string');
     res.send(hashedString);
 });
 
-app.post('/create-user', function (req, res) {
-   // username, password
-   // {"username": "tanmai", "password": "password"}
-   // JSON
-   var username = req.body.username;
-   var password = req.body.password;
-   var salt = crypto.randomBytes(128).toString('hex');
-   var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send('User successfully created: ' + username);
-      }
-   });
-});
-
-app.post('/login', function(req,res){
-    var username = req.body.username;
-    var password = req.body.password;
-   
-   pool.query('SELECT * FROM "user" WHERE  username = $1', [username], function(err,result){
-        if(err){
-           res.status(500).send();         
-       }
-       else {
-           if(result.rows.length === 0) {
-               res.send(403).send('username/password is invalid');
-           }else{
-               var dbString = result.rows[0].password;
-               var salt = dbString.split('$')[2];
-               var hashedPassword = hash(password, salt);
-               if (hashedPassword === dbString) {
-                   res.send('credentials correct');
-               }else {
-                   res.send(403).send('username/password is invalid');
-               }
-               
-           }
-           
-       }
-   }); 
-});
-
 var pool = new Pool(config); 
+
 app.get('/test-db', function(req,res){
     //return a response
    pool.query('SELECT *  FROM test', function(err,result){
        if(err){
-           res.status(500).send(err.toString());         
+           res.status(500).send(err.toString())
+;         
        }
+       
        else{
            res.send(JSON.stringify(result.rows));
        }
    }) ;
-});
+} );
 
 var counter=0;
 app.get('/counter',function(req , res){
@@ -205,6 +163,10 @@ app.get('/ui/madi2.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi2.png'));
 });
  
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
 app.listen(port, function () {
